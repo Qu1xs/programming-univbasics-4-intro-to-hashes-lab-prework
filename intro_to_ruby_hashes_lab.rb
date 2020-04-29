02f35663f9590e99c31ac0c8e3211acad0a90a94
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-29 03:06:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,8 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  key = :name
   hash = {
-     :name => value
+     :key => value
   }
 end
 
